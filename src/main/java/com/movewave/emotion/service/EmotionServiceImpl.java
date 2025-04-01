540212cc9b1e756c6ba58b6b1af2f59b76898aca
정윤환 <629jyh7@naver.com>
2025-04-01 16:11:00 +0900

    feat: flask 서버 호출 방식 변경(webFlux)

--- src/main/java/com/movewave/emotion/service/EmotionServiceImpl.java
+++ src/main/java/com/movewave/emotion/service/EmotionServiceImpl.java
@@ -3,9 +3,11 @@ package com.movewave.emotion.service;
 import com.movewave.emotion.model.response.EmotionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,35 +17,22 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class EmotionServiceImpl implements EmotionService {
 
+    private final WebClient webClient;
+
     @Override
     public EmotionResponse analyzeEmotion(String text) {
-        String flaskUrl = "http://localhost:5000/api/emotion/predict";
+        Map<String, String> requestBody = Map.of("text", text);
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        // Flask에 보낼 JSON body
-        Map<String, String> body = new HashMap<>();
-        body.put("text", text);
-
-        // HTTP 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // 요청 객체 생성
-        HttpEntity<Map<String, String>> entity = new HttpEntity<>(body, headers);
-
-        // Flask 서버로 요청 보내고 결과 받기
-        ResponseEntity<Map> response = restTemplate.exchange(
-                flaskUrl,
-                HttpMethod.POST,
-                entity,
-                Map.class
-        );
+        Map<String, Object> result = webClient.post()
+                .uri("/api/emotion/predict")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                .block(); // 동기 방식
 
         // 결과 파싱
-        Map result = response.getBody();
-        String prediction = (String) result.get("prediction");
-        Double confidence = ((Number) result.get("confidence")).doubleValue();
+        String prediction = String.valueOf(result.get("prediction"));
+        double confidence = ((Number)result.get("confidence")).doubleValue();
 
         return new EmotionResponse(prediction, confidence);
     }
