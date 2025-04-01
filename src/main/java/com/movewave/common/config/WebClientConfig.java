33daf3b372d0c02b257fd1f3852822e232965fc8
정윤환 <629jyh7@naver.com>
2025-04-01 16:11:00 +0900

    feat: flask 서버 호출 방식 변경(webFlux)

---
+++ src/main/java/com/movewave/common/config/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.movewave.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("http://localhost:5000")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
