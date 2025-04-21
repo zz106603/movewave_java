7a3e46f2a479cf751934d4d9501246292342ca1e
정윤환 <629jyh7@naver.com>
2025-04-21 18:01:34 +0900

    feat: 상수 문자열 수정

--- src/main/java/com/movewave/youtube/client/YouTubeApiClient.java
+++ src/main/java/com/movewave/youtube/client/YouTubeApiClient.java
@@ -11,18 +11,26 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class YouTubeApiClient {
 
     private final WebClient webClient;
+    private static final String SEARCH_PATH = "/search";
+    private static final String PART_PARAM = "part";
+    private static final String QUERY_PARAM = "q";
+    private static final String TYPE_PARAM = "type";
+    private static final String MAX_RESULTS_PARAM = "maxResults";
+    private static final String API_KEY_PARAM = "key";
+    private static final String VIDEO_TYPE = "video";
+    private static final String SNIPPET_PART = "snippet";
 
     public YouTubeApiClient(@Qualifier("youTubeWebClient") WebClient webClient) {
         this.webClient = webClient;
     }
 
     public YouTubeSearchResponse search(String query, int maxResults, String apiKey) {
-        String uri = UriComponentsBuilder.fromPath("/search")
-                .queryParam("part", "snippet")
-                .queryParam("q", query)
-                .queryParam("type", "video")
-                .queryParam("maxResults", maxResults)
-                .queryParam("key", apiKey)
+        String uri = UriComponentsBuilder.fromPath(SEARCH_PATH)
+                .queryParam(PART_PARAM, SNIPPET_PART)
+                .queryParam(QUERY_PARAM, query)
+                .queryParam(TYPE_PARAM, VIDEO_TYPE)
+                .queryParam(MAX_RESULTS_PARAM, maxResults)
+                .queryParam(API_KEY_PARAM, apiKey)
                 .build()
                 .toUriString();
 
