59fee19bb4c2925f288123e8c73d1fba3d92a7e0
정윤환 <629jyh7@naver.com>
2025-06-04 22:47:34 +0900

    feat: Virtual Threads 검토 필요

--- src/main/java/com/movewave/common/config/TomcatVirtualThreadConfig.java
+++ src/main/java/com/movewave/common/config/TomcatVirtualThreadConfig.java
@@ -12,6 +12,8 @@ public class TomcatVirtualThreadConfig {
 
     /**
      * Tomcat의 Connector에 Virtual Thread를 사용하는 Executor를 설정하는 메서드입니다.
+     * 
+     * FIXME: 현재 Virtual Threads가 프로젝트에 유용하게 작동하는지 검토 필요
      *
      * @return Virtual Thread를 사용하는 Executor를 설정하는 TomcatConnectorCustomizer
      */
